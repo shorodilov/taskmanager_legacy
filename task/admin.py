7d7f178ab079e18c6228a002ece702a9e3ede9e0
Serhii Horodilov <sgorodil@gmail.com>
2022-08-17 15:20:32 +0300

    Customized task admin with `admin.register` decorator

--- task/admin.py
+++ task/admin.py
@@ -13,28 +13,22 @@ def tasks_count(obj: ProjectModel | TaskTagModel) -> int:
     return obj.tasks.count()
 
 
+@admin.register(ProjectModel)
 class ProjectModelAdmin(admin.ModelAdmin):
     list_display = "title", "summary", tasks_count
     list_display_links = "title",  # note it is a tuple, not a string
     ordering = "title",
 
 
-admin.site.register(ProjectModel, ProjectModelAdmin)
-
-
+@admin.register(TaskTagModel)
 class TaskTagModelAdmin(admin.ModelAdmin):
     list_display = "name", tasks_count
     list_display_links = "name",
 
 
-admin.site.register(TaskTagModel, TaskTagModelAdmin)
-
-
+@admin.register(TaskModel)
 class TaskModelAdmin(admin.ModelAdmin):
     list_display = "title", "created_at", "updated_at"
     list_display_links = "title",
     ordering = "project", "title"
     list_filter = "project", "tag"
-
-
-admin.site.register(TaskModel, TaskModelAdmin)
