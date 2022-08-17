6c9f997892b17dbf8679f38e6a7f3f2a5b2fb81a
Serhii Horodilov <sgorodil@gmail.com>
2022-08-17 22:08:10 +0300

    Updated task views (CRUD)
    Added create and update implementations.

--- task/views.py
+++ task/views.py
@@ -3,10 +3,13 @@ Task application views
 
 """
 
-from django.http import Http404, HttpRequest, HttpResponse
+from django.http import Http404, HttpRequest, HttpResponse, \
+    HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.decorators.http import require_http_methods
 
+from task.forms import TaskForm
 from task.models import TaskModel
 
 
@@ -31,14 +34,41 @@ def task_detail_view(request: HttpRequest, pk: int) -> HttpResponse:
 def task_create_view(request: HttpRequest) -> HttpResponse:
     """Task create view implementation"""
 
-    raise NotImplementedError()
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            redirect_url = reverse("task:detail", args=(form.instance.pk,))
+
+            return HttpResponseRedirect(redirect_url)
+
+    else:
+        form = TaskForm()
+
+    return render(request, "task/task_form.html", {"form": form})
 
 
 @require_http_methods(["GET", "POST"])
 def task_update_view(request: HttpRequest, pk: int) -> HttpResponse:
     """Task update view implementation"""
 
-    raise NotImplementedError()
+    try:
+        task = TaskModel.objects.get(pk=pk)
+    except TaskModel.DoesNotExist:
+        raise Http404("Task not found")
+
+    if request.method == "POST":
+        form = TaskForm(instance=task, data=request.POST)
+        if form.is_valid():
+            form.save()
+            redirect_url = reverse("task:detail", args=(task.pk,))
+
+            return HttpResponseRedirect(redirect_url)
+
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, "task/task_form.html", {"form": form})
 
 
 @require_http_methods(["GET", "POST"])
