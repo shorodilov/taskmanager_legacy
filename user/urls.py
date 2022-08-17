db68e07e62b632caea4aeb85ca00a614c950f561
Serhii Horodilov <sgorodil@gmail.com>
2022-08-17 18:14:03 +0300

    Added user views
    Added user profile view.
    Set up boilerplates for auth views.

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,14 @@ User application URL Configuration
 
 """
 
+from django.urls import path
+
+from user import views
+
 app_name = "user"
-urlpatterns = []
+urlpatterns = [
+    path("signin/", views.signin_view, name="signin"),
+    path("signup/", views.signup_view, name="signup"),
+    path("logout/", views.logout_view, name="logout"),
+    path("profile/", views.user_detail_view, name="profile"),
+]
