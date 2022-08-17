a410d84e116e36b4cbb4ce98b0a09d61c6b536a5
Serhii Horodilov <sgorodil@gmail.com>
2022-08-17 17:04:34 +0300

    Added context example to views
    Updated task templates. Ready to move to extended templates.

--- task/views.py
+++ task/views.py
@@ -3,18 +3,29 @@ Task application views
 
 """
 
-from django.http import HttpRequest, HttpResponse
+from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
+
+# TODO: remove this after models implementation
+TASKS = {
+    1: {"task_id": 1, "title": "task #1", "completed": False},
+    2: {"task_id": 2, "title": "task #2", "completed": True},
+    3: {"task_id": 3, "title": "task #3", "completed": False},
+}
 
 
 def task_list_view(request: HttpRequest) -> HttpResponse:
     """Task list view implementation"""
 
-    return render(request, "task/task_list.html")
+    ctx = {"object_list": TASKS.values()}
+    return render(request, "task/task_list.html", ctx)
 
 
 def task_detail_view(request: HttpRequest, pk: int) -> HttpResponse:
     """Task detail view implementation"""
 
-    ctx = {"task_id": pk}
-    return render(request, "task/task_detail.html", ctx)
+    try:
+        ctx = {"object": TASKS[pk]}
+        return render(request, "task/task_detail.html", ctx)
+    except KeyError:
+        raise Http404("Task not found")
